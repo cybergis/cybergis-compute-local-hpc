15e396264b1ee1c69be33fb2a35b1e48dec9d444
Mit <53411468+mitkotak@users.noreply.github.com>
2022-05-27 20:56:33 -0500

    stuffed everything into one container ( Needs housekeeping )

--- docker/slurm-docker-cluster/docker-entrypoint.sh
+++ docker/slurm-docker-cluster/docker-entrypoint.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/dumb-init /bin/sh
+#!/bin/bash
 set -e
 
 if [ "$1" = "slurmdbd" ]
@@ -10,6 +10,7 @@ then
 
     {
         . /etc/slurm/slurmdbd.conf
+        sleep 5
         until echo "SELECT 1" | mysql -h $StorageHost -u$StorageUser -p$StoragePass 2>&1 > /dev/null
         do
             echo "-- Waiting for database to become active ..."
@@ -58,34 +59,3 @@ then
 fi
 
 exec "$@"
-
-# openconnect
-if [[ ! -z "${OPENCONNECT_USER}" ]]; then
-  if [[ -z "${OPENCONNECT_PASSWORD}" ]]; then
-    # Ask for password
-    openconnect -b -u $OPENCONNECT_USER --protocol=anyconnect --authgroup $OPENCONNECT_AUTHGROUP $OPENCONNECT_URL
-  elif [[ ! -z "${OPENCONNECT_PASSWORD}" ]] && [[ ! -z "${OPENCONNECT_MFA_CODE}" ]]; then
-    # Multi factor authentication (MFA)
-    (echo $OPENCONNECT_PASSWORD; echo $OPENCONNECT_MFA_CODE) | openconnect -b -u $OPENCONNECT_USER  --protocol=anyconnect --authgroup $OPENCONNECT_AUTHGROUP --passwd-on-stdin $OPENCONNECT_URL
-  elif [[ ! -z "${OPENCONNECT_PASSWORD}" ]]; then
-    # Standard authentication
-    echo $OPENCONNECT_PASSWORD | openconnect -b -u $OPENCONNECT_USER --protocol=anyconnect --authgroup $OPENCONNECT_AUTHGROUP --passwd-on-stdin $OPENCONNECT_URL
-  fi
-fi
-
-# install necessary packages
-npm install -g typescript
-cd /job_supervisor && npm install
-
-# compile TypeScript
-cd /job_supervisor && npm run build
-
-# set SSH config
-cat >> /etc/ssh_config <<EOT
-Host *
-    StrictHostKeyChecking no
-EOT
-
-# run server
-redis-server /job_supervisor/docker/redis.conf --daemonize yes
-node /job_supervisor/production/server.js
